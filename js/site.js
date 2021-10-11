21e2eb3bd4c430c7e1a5e2982688cddfbd6c548a
Cwatson87 <snivley12@gmail.com>
2021-10-11 03:08:37 -0400

    debugging

--- js/site.js
+++ js/site.js
@@ -1,42 +1,32 @@
-
 // displays a message to the user
-function getString(){
+function getString() {
     //Get the user's input
-let userString = document.getElementById("userString").value
+    let userString = document.getElementById("userString").value
     //Version 2 - Test the user's input for length
- 
+
     //Reverse the user's input - this is a separate function
- let revString = reverseString(userString)
+    let revString = reverseString(userString)
 
     //Display the result
-displayString(revString);
+    displayString(revString);
 }
 
-function reverseString(userString){
-    let startValue = userString.length-1;
+function reverseString(userString) {
+    let startValue = userString.length - 1;
     let endValue = 0;
-
-    //User input = Frank
-    //Frank is a string and an array of characters
-    //[F,r,a,n,k]
 
     let revString = "";
 
     //In a decremented for loop the startValue must be higher than the endValue
-    for(let i = startValue; i >= endValue; i--){
+    for (let i = startValue; i >= endValue; i--) {
         revString += userString[i];
     };
 
-    //loop 1 - "k"
-    //loop 2 - "kn"
-    //loop 3 - "kna"
-    //loop 4 - "knar"
-    //loop 5 - "knarF"
     return revString
 
 }
 
-function displayString(revString){
+function displayString(revString) {
     let output = document.getElementById("results")
 
     output.innerHTML = revString;
